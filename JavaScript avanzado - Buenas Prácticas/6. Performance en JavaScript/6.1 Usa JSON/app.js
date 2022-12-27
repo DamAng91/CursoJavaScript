42265a706cdace0c4b51c8059e3349d3e1807e4a
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:37:00 -0300

    Create app.js

---
+++ JavaScript avanzado - Buenas Prácticas/6. Performance en JavaScript/6.1 Usa JSON/app.js
@@ -0,0 +1,11 @@
+//$.getJSON("productos.json", function(productos) {
+var url = "dirección del clima";
+$.getJSON(url, function(productos) {
+    console.log(productos);
+
+/*    var totalProductos = productos.length;
+
+    for (var i = 0; i < totalProductos; i++) {
+        console.log(productos[i].nombre);
+    }*/
+})
