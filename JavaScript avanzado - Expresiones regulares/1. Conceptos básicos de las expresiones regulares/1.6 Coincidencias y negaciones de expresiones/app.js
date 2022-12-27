4312891ad5a841b6345e258fb7f73edf5c54024d
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:38:13 -0300

    Create app.js

---
+++ JavaScript avanzado - Expresiones regulares/1. Conceptos básicos de las expresiones regulares/1.6 Coincidencias y negaciones de expresiones/app.js
@@ -0,0 +1,8 @@
+//var expresion1 = /[\d]/; //Evalua si al menos existe un numero
+
+//console.log(expresion1.test('29-06-2007'));
+
+expresion2 = /[^01]/; //Es una evaluación de negación ^
+
+console.log(expresion2.test('10101110110001001'));
+
