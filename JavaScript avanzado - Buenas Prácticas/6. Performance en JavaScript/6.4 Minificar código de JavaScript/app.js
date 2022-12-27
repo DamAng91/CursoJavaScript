e2209162555634ff9ab3aa082849efdfea324155
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:37:23 -0300

    Create app.js

---
+++ JavaScript avanzado - Buenas Prácticas/6. Performance en JavaScript/6.4 Minificar código de JavaScript/app.js
@@ -0,0 +1,24 @@
+/*var tienda = {
+	nombre: "Tienda las 4 esquinas",
+	calcular: function(costo1, costo2) {
+		return costo1 + costo2;
+	},
+	saludar: function() {
+		var mensaje = "Hola";
+		return mensaje;
+	}
+};
+
+var btnSaludar = document.getElementById("btnSaludar");
+
+btnSaludar.addEventListener('click', function() {
+	console.log(tienda.saludar());
+}); 
+
+var tienda={nombre:"Tienda las 4 esquinas",calcular:function(n,a){return n+a},saludar:function(){return"Hola"}},btnSaludar=document.getElementById("btnSaludar");btnSaludar.addEventListener("click",function(){console.log(tienda.saludar())});
+*/
+var _0xa449=["\x54\x69\x65\x6E\x64\x61\x20\x6C\x61\x73\x20\x34\x20\x65\x73\x71\x75\x69\x6E\x61\x73","\x48\x6F\x6C\x61","\x62\x74\x6E\x53\x61\x6C\x75\x64\x61\x72","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x6C\x69\x63\x6B","\x73\x61\x6C\x75\x64\x61\x72","\x6C\x6F\x67","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72"];var tienda={nombre:_0xa449[0],calcular:function(_0xd344x2,_0xd344x3){return _0xd344x2+ _0xd344x3},saludar:function(){var _0xd344x4=_0xa449[1];return _0xd344x4}};var btnSaludar=document[_0xa449[3]](_0xa449[2]);btnSaludar[_0xa449[7]](_0xa449[4],function(){console[_0xa449[6]](tienda[_0xa449[5]]())})
+
+console.log(tienda.nombre);
+console.log(tienda.calcular(20,32));
+console.log(tienda.saludar());
