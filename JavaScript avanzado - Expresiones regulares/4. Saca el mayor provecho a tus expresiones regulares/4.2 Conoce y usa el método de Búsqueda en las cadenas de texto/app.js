97c92628acb797ed59ab02051760438cda143a44
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:39:08 -0300

    Create app.js

---
+++ JavaScript avanzado - Expresiones regulares/4. Saca el mayor provecho a tus expresiones regulares/4.2 Conoce y usa el método de Búsqueda en las cadenas de texto/app.js
@@ -0,0 +1,8 @@
+var mensaje = 'Esta historia continuará...';
+
+//  \w Cualquier caracter que no sea alfanumérico.
+//  \s Cualquier caracter que no sea sea un espacio en blanco.
+
+//console.log(mensaje.search(/\w/)); //Consola imprime el valor de la posición donde se encontró el primer caracter.
+
+console.log(mensaje.search(/\s/)); //Consola imprime el valor de la posición donde encuentra el primer caracter.
