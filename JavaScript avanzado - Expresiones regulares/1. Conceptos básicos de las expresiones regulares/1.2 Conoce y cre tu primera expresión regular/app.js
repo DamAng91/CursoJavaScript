58805f42df0783982717ca50760177c6102409b0
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:37:50 -0300

    Create app.js

---
+++ JavaScript avanzado - Expresiones regulares/1. Conceptos básicos de las expresiones regulares/1.2 Conoce y cre tu primera expresión regular/app.js
@@ -0,0 +1,5 @@
+var expresion1 = new RegExp('abc'); //Objeto para crear expresiones regulares. Recibe como parámetro la evaluación que nosotros queremos hacer.
+
+var expresion2 = /abc/; //Otra expresión para crear una expresion regular.
+
+//var parraf = /text1 \/n /tex2/; //Otra manera de expresar una expresión regular. 
