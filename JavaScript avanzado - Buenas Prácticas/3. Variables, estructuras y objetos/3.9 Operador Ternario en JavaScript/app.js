74dd84d36d2695b5afc2567a5cc31a44ec926801
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:35:07 -0300

    Create app.js

---
+++ JavaScript avanzado - Buenas Prácticas/3. Variables, estructuras y objetos/3.9 Operador Ternario en JavaScript/app.js
@@ -0,0 +1,10 @@
+var estado = "true";
+
+/*if (estado) {
+    console.log("continuar");
+} else {
+    console.log("detenerse");
+}*/
+
+var accion = estado ? 'continuar' : 'detenerse'; //Si estado es V entonces "continuar" sino "detenerse"
+
