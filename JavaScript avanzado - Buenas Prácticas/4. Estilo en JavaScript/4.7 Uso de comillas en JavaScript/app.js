c5b3237881e696e48118e716a91226297e034247
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:35:41 -0300

    Create app.js

---
+++ JavaScript avanzado - Buenas Prácticas/4. Estilo en JavaScript/4.7 Uso de comillas en JavaScript/app.js
@@ -0,0 +1,5 @@
+var mensaje = "La actividad estuvo 'divertida'";
+var mensajeCompleto = 'La actividad estuvo "divertida"'; //En este caso, 'divertida' JS lo toma como si fuera una variable, por lo tanto, producirá un error.
+
+console.log(mensaje);
+console.log(mensajeCompleto);
