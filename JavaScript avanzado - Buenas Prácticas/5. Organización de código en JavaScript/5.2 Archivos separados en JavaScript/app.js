a51d13e390fdb340603d6c62425289bda39f8713
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:36:06 -0300

    Create app.js

---
+++ JavaScript avanzado - Buenas Prácticas/5. Organización de código en JavaScript/5.2 Archivos separados en JavaScript/app.js
@@ -0,0 +1,6 @@
+/*
+
+Siempre es importante mantener bien organizado nuestro código dado que generalmente vamos a ocupar muchísimos archivos, tanto de JS como CSS o HTML, por lo que es conveniente, realizar una separación adecuada y tengamos carpetas exclusivas para cada lenguaje y/o librerías “libs”. Esto permite que el código de JavaScript se va a trabajar de manera independiente. Esta separación se archivos se denomina “módulización”. 
+
+
+*/
