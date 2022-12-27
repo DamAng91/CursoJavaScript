bf520688ea1f33b386d94eddb4569a39f6b82672
DamAng91 <damian.angelucci@hotmail.com>
2022-12-26 21:34:16 -0300

    Create app.js

---
+++ JavaScript avanzado - Buenas Prácticas/3. Variables, estructuras y objetos/3.13 Literales en JavaScript/app.js
@@ -0,0 +1,13 @@
+var persona = {};
+persona.nombre = "Damian";
+
+var otraPersona = new Object(); //Declaramos un Objeto
+
+
+var lista = new Array(); //Declaramos un Arreglo
+lista[0] = "manzana";
+lista [1] = "pera";
+
+
+var listaNueva = ["manzana", "pera"];
+
